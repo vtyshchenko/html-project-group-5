195620102e98e93630055407d71b190b3e3c5dbc
vtyshchenko <30977223+vtyshchenko@users.noreply.github.com>
2021-07-04 19:37:15 +0000

    Deploying to gh-pages from @ vtyshchenko/html-project-group-5@78d99a36e038c382d8f2e0c73a8d277f59d36c21 🚀

--- src.e2b5698f.js
+++ src.e2b5698f.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/campfire.svg":[["campfire.998ec590.svg","gFvl"],"gFvl"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/html-project-group-5/src.722240f9.js.map
+//# sourceMappingURL=/html-project-group-5/src.e2b5698f.js.map
