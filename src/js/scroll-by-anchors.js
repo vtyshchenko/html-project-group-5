ba821497b283252899eba2d1b90ce462e01c1afa
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-07-08 19:26:27 +0300

    bugfix js

--- src/js/scroll-by-anchors.js
+++ src/js/scroll-by-anchors.js
@@ -4,8 +4,16 @@ const mobileMenuRef = document.querySelector("[data-menu]");
     
 for (let anchor of anchors) {
     anchor.addEventListener("click", function (scroll) {
-      document.body.classList.toggle("modal-open");
-      mobileMenuRef.classList.toggle("is-open");
+        console.log(document.body.classList);
+        console.log(mobileMenuRef.classList);
+
+        if (document.body.classList.value == "modal-open") {
+            document.body.classList.toggle("modal-open");
+        }
+
+        if (mobileMenuRef.classList.value == "mobile-menu is-open") {
+            mobileMenuRef.classList.toggle("is-open");
+        }
         scroll.preventDefault();
         const blockID = anchor.getAttribute('href')
         document.querySelector('' + blockID).scrollIntoView({
@@ -14,3 +22,6 @@ for (let anchor of anchors) {
         })
     })
 }
+
+// DOMTokenList ["modal-open", value: "modal-open"]
+// scroll-by-anchors.js:8 DOMTokenList(2) ["mobile-menu", "is-open", value: "mobile-menu is-open"]
