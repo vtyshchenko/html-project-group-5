10e8f0d7a70c706e9204263f77908527e88b2ce4
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-08-28 22:16:26 +0300

    bugfix

--- src/js/main.js
+++ src/js/main.js
@@ -1,87 +1,61 @@
+let slideIndex = 1;
+showSlides(slideIndex);
 
-      function plusSlides(n) {
-        showSlides((slideIndex += n));
-      }
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-      function currentSlide(n) {
-        showSlides((slideIndex = n));
-      }
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
-      function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName('reviews__slides');
-        var dots = document.getElementsByClassName('demo');
-        var miniat = document.getElementsByClassName('column');
-        if (n > slides.length) {
-          slideIndex = 1;
-        }
-        if (n < 1) {
-          slideIndex = slides.length;
-        }
-        for (i = 0; i < dots.length; i++) {
-          some_text = { index: index };
-          console.log(some_text);
-          dots[i].className = dots[i].className.replace(' active', '');
-          var index = dots[i].className.indexOf(' visually-hidden');
-          if (index < 0) {
-            dots[i].className += ' visually-hidden';
-          }
-        }
+function showSlides(n) {
+  const slides = document.getElementsByClassName('reviews__slides');
+  const dots = document.getElementsByClassName('demo');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < dots.length; i++) {
+    some_text = { index: index };
+    dots[i].className = dots[i].className.replace(' active', '');
+    const index = dots[i].className.indexOf(' visually-hidden');
+    if (index < 0) {
+      dots[i].className += ' visually-hidden';
+    }
+  }
 
-        d = slideIndex - 1;
-        if (d < 1) {
-          d = 1;
-        } else {
-          if (d > dots.length - 2) {
-            d = dots.length - 2;
-          }
-        }
+  d = slideIndex - 1;
+  if (d < 1) {
+    d = 1;
+  } else {
+    if (d > dots.length - 2) {
+      d = dots.length - 2;
+    }
+  }
 
-        for (i = -1; i < 2; i++) {
-          dots[d - i].className = dots[d - 1].className.replace(' visually-hidden', '');
-        }
+  for (let i = -1; i < 2; i += 1) {
+    dots[d - i].className = dots[d - 1].className.replace(' visually-hidden', '');
+  }
 
-        for (i = 0; i < slides.length; i++) {
-          slides[i].className = slides[i].className.replace(' visually-hidden', '');
+  for (let i = 0; i < slides.length; i += 1) {
+    slides[i].className = slides[i].className.replace(' visually-hidden', '');
 
-          if (i != n - 1) {
-            slides[i].className += ' visually-hidden';
-          }
-        }
+    if (i != n - 1) {
+      slides[i].className += ' visually-hidden';
+    }
+  }
 
-        slides[slideIndex - 1].className = slides[slideIndex - 1].className.replace(
-          ' visually-hidden',
-          '',
-        );
-        dots[slideIndex - 1].className += ' active';
-      }
+  slides[slideIndex - 1].className = slides[slideIndex - 1].className.replace(
+    ' visually-hidden',
+    '',
+  );
+  dots[slideIndex - 1].className += ' active';
+}
 
-      function showMore(id) {
-        document.getElementById(id + 'Overflow').className = '';
-        document.getElementById(id + 'MoreLink').className = 'link-text-toggle hidden';
-        document.getElementById(id + 'dots').className = 'hidden';
-        document.getElementById(id + 'LessLink').className = 'link-text-toggle';
-      }
-
-      function showLess(id) {
-        document.getElementById(id + 'Overflow').className = 'hidden';
-        document.getElementById(id + 'MoreLink').className = 'link-text-toggle';
-        document.getElementById(id + 'LessLink').className = 'link-text-toggle hidden';
-      }
-
-      var slideIndex = 1;
-      showSlides(slideIndex);
-
-      var len = 1000;
-      var shrinkables = document.getElementsByClassName('shrinkable');
-      if (shrinkables.length > 0) {
-        for (var i = 0; i < shrinkables.length; i++) {
-          var fullText = shrinkables[i].innerHTML;
-          if (fullText.length > len) {
-            var trunc = fullText.substring(0, len).replace(/\w+$/, '');
-            var remainder = '';
-            var id = shrinkables[i].id;
-            remainder = fullText.substring(len, fullText.length);
-          }
-        }
-      }
+// document.querySelector('button').addEventListener('click', function () {
+//   document.querySelector('#content').style.height = 'auto';
+//   this.style.display = 'none';
+// });
--- src/js/scroll-by-anchors.js
+++ src/js/scroll-by-anchors.js
@@ -1,26 +1,23 @@
 const anchors = document.querySelectorAll('a[href*="#_"]');
-const mobilelinkClick = document.querySelector("[data-menu-close]");
-const mobileMenuRef = document.querySelector("[data-menu]");
-    
+const mobilelinkClick = document.querySelector('[data-menu-close]');
+const mobileMenuRef = document.querySelector('[data-menu]');
+
 for (let anchor of anchors) {
-    anchor.addEventListener("click", function (scroll) {
-        console.log(document.body.classList);
-        console.log(mobileMenuRef.classList);
+  anchor.addEventListener('click', function (scroll) {
+    if (document.body.classList.value == 'modal-open') {
+      document.body.classList.toggle('modal-open');
+    }
 
-        if (document.body.classList.value == "modal-open") {
-            document.body.classList.toggle("modal-open");
-        }
-
-        if (mobileMenuRef.classList.value == "mobile-menu is-open") {
-            mobileMenuRef.classList.toggle("is-open");
-        }
-        scroll.preventDefault();
-        const blockID = anchor.getAttribute('href')
-        document.querySelector('' + blockID).scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        })
-    })
+    if (mobileMenuRef.classList.value == 'mobile-menu is-open') {
+      mobileMenuRef.classList.toggle('is-open');
+    }
+    scroll.preventDefault();
+    const blockID = anchor.getAttribute('href');
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
 }
 
 // DOMTokenList ["modal-open", value: "modal-open"]
